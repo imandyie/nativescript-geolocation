7b6c3f416e506baeba616b59374cb07a53c4353b
Andy <voxxxlll@gmail.com>
2021-06-01 12:14:22 +0100

    move from src to root

--- demo/references.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="./node_modules/@nativescript/core/references.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-android/index.d.ts" />
